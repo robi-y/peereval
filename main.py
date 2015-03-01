80a6104f78e82cd85cb2b5b385bd06185f64baa0
Reuven Yagel <robi@post.jce.ac.il>
2015-03-01 15:05:40 +0200

    using Jinja template

--- main.py
+++ main.py
@@ -5,14 +5,28 @@ import webapp2
 from google.appengine.api import users
 #see http://stackoverflow.com/a/22275563/4388 for oauth with github
 
+import os
+import jinja2
+
+jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
         if user is not None:
-            self.response.write('Hello PeerEval!')
+            logout_url = users.create_logout_url(self.request.uri)
+            template_context = {
+                'user': user.nickname(),
+                'logout_url': logout_url,
+            }
+            template = jinja_env.get_template('main.html')
+            self.response.out.write(
+                template.render(template_context))
         else:
             login_url = users.create_login_url(self.request.uri)
             self.redirect(login_url)
+
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
