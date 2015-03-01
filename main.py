3257f7f870386796ae918f9ab407c3ab0fb1d27d
Reuven Yagel <robi@post.jce.ac.il>
2015-03-01 14:55:00 +0200

    basic auth with users service

--- main.py
+++ main.py
@@ -1,25 +1,18 @@
 #!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+
 import webapp2
+
+from google.appengine.api import users
+#see http://stackoverflow.com/a/22275563/4388 for oauth with github
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello JCE!')
-
+        user = users.get_current_user()
+        if user is not None:
+            self.response.write('Hello PeerEval!')
+        else:
+            login_url = users.create_login_url(self.request.uri)
+            self.redirect(login_url)
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
