6788a4d642277db17ae815e4527cca39cf3aa26a
robi-y <robi@post.jce.ac.il>
2015-03-02 18:09:47 +0200

    filter evaluated peers!

--- main.py
+++ main.py
@@ -15,16 +15,17 @@ from utils import Utils
 jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
-class MainHandler(webapp2.RequestHandler):
+class PeerEval(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
+        _username = 'student1' # query User
         _iteration = 0 #query Group
         if user is not None:
             logout_url = users.create_logout_url(self.request.uri)
             template_context = {
                 'user': user.nickname(),
                 'logout_url': logout_url,
-                'remaining_peers': self.ramaining_peers(user, _iteration)
+                'remaining_peers': self.ramaining_peers(_username, _iteration)
             }
             self._render_template(template_context)
         else:
@@ -36,8 +37,8 @@ class MainHandler(webapp2.RequestHandler):
         if user is None:
             self.error(401)
 
-        _username = 'jce.student' # query User
-        _groupname = 'BestBuy' #query Group
+        _username = 'student1' # query User
+        _groupname = 'group1' #query Group
         _iteration = 0 #query Group
         evaluation = Evaluation(username=_username,
                 groupname=_groupname,
@@ -56,7 +57,7 @@ class MainHandler(webapp2.RequestHandler):
             'user': user.nickname(),
             'logout_url': logout_url,
             'eval_peer': self.request.get('peer'),
-            'remaining_peers':  self.ramaining_peers(user.email(), _iteration)
+            'remaining_peers':  self.ramaining_peers(_username, _iteration)
         }
         self._render_template(template_context)
     
@@ -65,11 +66,36 @@ class MainHandler(webapp2.RequestHandler):
         self.response.out.write(
             template.render(context))
         
-    def ramaining_peers(self, user, iteration):
-        peers = User.qry_peers(user) #.fetch()
-        evaluated = [peer.groupname for peer in Evaluation.qry_evaluated_peers(user.email(), iteration).fetch()]
+    def ramaining_peers(self, username, iteration):
+        peers = [user.username for user in User.qry_peers(username).fetch()]
+        evaluated = [evaluation.peer for evaluation in Evaluation.qry_evaluated_peers(username, iteration).fetch()]
         return Utils.complements(peers, evaluated)
 
+class Populator(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            self.response.out.write('Welcome, %s!' % user.nickname())
+        if users.is_current_user_admin():
+#            evaluation = Evaluation(username='student1',
+#                groupname='group1',
+#                iteration=0,
+#                peer='student3',
+#                grade=-3,
+#                justification='did not contribute'
+#            )
+#            evaluation.put()
+            student = User(username='student1', groupname='group1')
+            student.put()
+            student = User(username='student2', groupname='group1')
+            student.put()
+            student = User(username='student3', groupname='group1')
+            student.put()
+            student = User(username='student4', groupname='group1')
+            student.put()
+            self.response.out.write('\nDone populating!')
+            
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', PeerEval),
+    ('/populate', Populator)
 ], debug=True)
